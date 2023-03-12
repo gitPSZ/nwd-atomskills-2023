89be84b69a13230ab547febc213885f95ebaabb1
AJHoll <anthony_j_holl@mail.ru>
2023-03-12 20:34:11 +0300

    change banner finish dump and edit version

--- database/before-restore.sql
+++ database/before-restore.sql
@@ -1,5 +1,11 @@
 create role uni_user inherit nologin;
-
 create role uni_admin inherit nologin;
-
 grant uni_user to uni_admin;
+
+create role "camunda-admin" with login;
+grant uni_admin TO "camunda-admin";
+alter role "camunda-admin" with password 'camunda-admin';
+
+create role "nwd-atomskills-2023" with login;
+grant uni_admin TO "nwd-atomskills-2023";
+alter role "nwd-atomskills-2023" with password 'nwd-atomskills-2023';
