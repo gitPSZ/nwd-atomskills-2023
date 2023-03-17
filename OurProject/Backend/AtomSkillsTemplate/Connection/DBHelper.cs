428a09d349fec50dce953a02414c151f5e5c9273
John Doe <nfhyev11@gmail.com>
2023-03-17 15:06:46 +0500

    - loadMachines

--- OurProject/Backend/AtomSkillsTemplate/Connection/DBHelper.cs
+++ OurProject/Backend/AtomSkillsTemplate/Connection/DBHelper.cs
@@ -5,6 +5,7 @@
         public static string Schema = "\"atom\"";
         public static string Requests = "\"request\"";
         public static string Contractors = "\"contractor\"";
+        public static string Machines = "\"machine\"";
         public static string Products = "\"product\"";
         public static string RequestPositions = "\"request_position\"";
     }
