1919c5227ec370d9bf66e758bf07381ff0b8b94a
John Doe <nfhyev11@gmail.com>
2023-03-18 16:35:25 +0500

    - monitoring

--- OurProject/Backend/AtomSkillsTemplate/Services/MonitoringService.cs
+++ OurProject/Backend/AtomSkillsTemplate/Services/MonitoringService.cs
@@ -6,6 +6,7 @@ using Dapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace AtomSkillsTemplate.Services
 {
@@ -13,6 +14,7 @@ namespace AtomSkillsTemplate.Services
     {
         IConnectionFactory connectionFactory;
         List<RequestForMonitoring> requestRepository;
+        List<MachineWrapper> machineWrappers;
         public MonitoringService(IConnectionFactory connectionFactory)  
         {
             this.connectionFactory = connectionFactory;
@@ -36,6 +38,17 @@ namespace AtomSkillsTemplate.Services
             {
                 try
                 {
+                    var machinesThatCanProcess = await connection.QueryAsync<Machine>(
+                        $"select * from {DBHelper.Schema}.{DBHelper.Machines} where id in (select id_machine from " +
+                        $" {DBHelper.Schema}.{DBHelper.MachineRequest} where id_request = :idRequest)", new { idRequest = request.Id });
+
+                    request.MachinesThatCanProcessThisGoddamnThing = new List<Machine>();
+
+                    if (machinesThatCanProcess != null && machinesThatCanProcess.Any())
+                    {
+                        request.MachinesThatCanProcessThisGoddamnThing = machinesThatCanProcess.ToList();
+                    }
+
                     request.RequestPositions = new List<RequestPositionForMonitoring>();
                     var requestPositionsInRequest = requestPositions.Where(o => o.RequestId == request.Id);
                     request.RequestPositions.AddRange(requestPositionsInRequest);
@@ -47,7 +60,74 @@ namespace AtomSkillsTemplate.Services
                 }
                 
             }
+            StartMachines();
         }
+        public async void StartMachines()
+        {
+            machineWrappers = new List<MachineWrapper>();
+            using var connection = connectionFactory.GetConnection();
+            var machines = await connection.QueryAsync<Machine>($"select * from {DBHelper.Schema}.{DBHelper.Machines}");
+            foreach(var machine in machines)
+            {
+                var wrapper = new MachineWrapper
+                {
+                    Machine = machine,
+                    ShouldStop = false,
+                    MonitoringTask = Task.Run(async () => { await ProcessEquipment(machine); })
+                };
+                machineWrappers.Add(wrapper);
+            }
+        }
+        public async Task ProcessEquipment(Machine machine)
+        {
+            Console.WriteLine("Начался опрос оборудования с ID = " + machine.Id);
+            while (true)
+            {
+
+                try
+                {
+                    if (machineWrappers.FirstOrDefault(o=>o.Machine.Id == machine.Id).ShouldStop)
+                    {
+                        machineWrappers.Remove(machineWrappers.FirstOrDefault(o => o.Machine.Id == machine.Id));
+                        break;
+                    }
+                    var requestsAssignedToThisMachine = new List<RequestForMonitoring>();
+                    lock (requestRepository)
+                    {
+                        var assignees = requestRepository.Where(o => o.MachinesThatCanProcessThisGoddamnThing.FirstOrDefault(p => p.Id == machine.Id) != null).ToList();
+                        if (assignees != null && assignees.Any())
+                        {
+                            requestsAssignedToThisMachine = assignees.ToList();
+                        }
+                    }
+
+                    if (requestsAssignedToThisMachine == null || requestsAssignedToThisMachine.Any() == false)
+                    {
+                        Console.WriteLine($"У машины {machine.Id} пустая очередь, ожидаем заявок");
+                        await Task.Delay(10000);
+                        continue;
+                    }
+
+                    lock (requestRepository)
+                    {
+                        var orderToProcess = requestRepository.OrderBy(o => o.Priority).FirstOrDefault();
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Ошибка в опросе оборудования: " + e.ToString());
+                }
+                
+            }
+        }
+    }
+    
+    public class MachineWrapper
+    {
+        public Machine Machine { get; set; }
+
+        public bool ShouldStop { get; set; }
+        public Task MonitoringTask { get; set; }
     }
     public class RequestForMonitoring
     {
@@ -60,8 +140,10 @@ namespace AtomSkillsTemplate.Services
         public string StateCode { get; set; }
         public string StateCaption { get; set; }
         public string ContractorName { get; set; }
+        public int Priority { get; set; }
 
         public List<RequestPositionForMonitoring> RequestPositions { get; set; }
+        public List<Machine> MachinesThatCanProcessThisGoddamnThing { get; set; }
 
     }
     public class RequestPositionForMonitoring
